5654838cdd793cdeb9aef2823934706b2867558f
RoeeRabinovich <roeerabinovitz@gmail.com>
2025-06-09 16:35:10 +0300

    updated footer

--- flowbite-react-app/src/components/Footer.tsx
+++ flowbite-react-app/src/components/Footer.tsx
@@ -1,32 +1,44 @@
 import {
   Footer,
-  FooterBrand,
   FooterCopyright,
   FooterDivider,
   FooterLink,
   FooterLinkGroup,
 } from "flowbite-react";
+import { useSelector } from "react-redux";
+import { TRootState } from "../../store/store";
 
 export function MyFooter() {
+  const user = useSelector((state: TRootState) => state.userSlice.user);
+
   return (
-    <Footer container>
+    <Footer container className="rounded-none">
       <div className="w-full text-center">
         <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
-          <FooterBrand
-            href="https://flowbite.com"
-            src="https://flowbite.com/docs/images/logo.svg"
-            alt="Flowbite Logo"
-            name="Flowbite"
-          />
           <FooterLinkGroup>
-            <FooterLink href="#">About</FooterLink>
-            <FooterLink href="#">Privacy Policy</FooterLink>
-            <FooterLink href="#">Licensing</FooterLink>
-            <FooterLink href="#">Contact</FooterLink>
+            <FooterLink href="/about">About</FooterLink>
+            {user ? (
+              <>
+                <FooterLink href="/home">Home</FooterLink>
+                <FooterLink href="/create-card">Create Card</FooterLink>
+                <FooterLink href="/card-editor">My Cards</FooterLink>
+                <FooterLink href="/favourites">Favorites</FooterLink>
+                <FooterLink href="/profile">Profile</FooterLink>
+              </>
+            ) : (
+              <>
+                <FooterLink href="/login">Login</FooterLink>
+                <FooterLink href="/register">Register</FooterLink>
+              </>
+            )}
           </FooterLinkGroup>
         </div>
         <FooterDivider />
-        <FooterCopyright href="#" by="Flowbite™" year={2022} />
+        <FooterCopyright
+          href="#"
+          by="Roee Rabinovich™"
+          year={new Date().getFullYear()}
+        />
       </div>
     </Footer>
   );
