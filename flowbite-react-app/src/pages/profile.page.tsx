7bb23bb545ce1069f7bc779ba572d24cab3d9c4c
RoeeRabinovich <roeerabinovitz@gmail.com>
2025-06-09 16:25:27 +0300

    added back button to profile

--- flowbite-react-app/src/pages/profile.page.tsx
+++ flowbite-react-app/src/pages/profile.page.tsx
@@ -1,16 +1,23 @@
 import { useSelector } from "react-redux";
 import { TRootState } from "../../store/store";
 import MapView from "../components/MapView";
+import { Button } from "flowbite-react";
+import { IoIosReturnLeft } from "react-icons/io";
+import { useNavigate } from "react-router";
 
 const Profile = () => {
   const user = useSelector((state: TRootState) => state.userSlice.user);
+  const navigate = useNavigate();
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start gap-2 pt-5 dark:bg-gray-900">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-2 pt-5 dark:bg-gray-900">
       <h1 className="text-2xl dark:text-white">Profile Page</h1>
       <p className="text-lg dark:text-white">Welcome {user?.name.first}</p>
 
       <div className="mx-auto w-xl space-y-6 rounded-2xl bg-white p-6 text-center shadow-lg dark:bg-gray-800">
+        <Button onClick={() => navigate(-1)}>
+          <IoIosReturnLeft className="text-2xl" /> Back
+        </Button>
         <img
           src={user?.image.url}
           alt={`${user?.name.first} ${user?.name.last} picture`}
@@ -19,17 +26,19 @@ const Profile = () => {
         <h2 className="text-3xl font-semibold text-gray-800 dark:text-gray-200">
           {user?.name.first} {user?.name.last}
         </h2>
-        <p className="text-lg text-gray-600 dark:text-gray-200">
-          <strong>Email:</strong> {user?.email}
-        </p>
-        <p className="text-lg text-gray-600 dark:text-gray-200">
-          <strong>Phone:</strong> {user?.phone}
-        </p>
-        <p className="text-lg text-gray-600 dark:text-gray-200">
-          <strong>Address:</strong>
-          <br />
-          {`${user?.address.street} ${user?.address.houseNumber}, ${user?.address.city}`}
-        </p>
+        <ul className="list-none space-y-4 text-start">
+          <li className="text-lg text-gray-600 dark:text-gray-200">
+            <strong>Email:</strong> {user?.email}
+          </li>
+          <li className="text-lg text-gray-600 dark:text-gray-200">
+            <strong>Phone:</strong> {user?.phone}
+          </li>
+          <li className="text-lg text-gray-600 dark:text-gray-200">
+            <strong>Address:</strong>
+            <br />
+            {`${user?.address.street} ${user?.address.houseNumber}, ${user?.address.city}`}
+          </li>
+        </ul>
         {user && (
           <div className="mt-4 w-full max-w-4xl">
             <MapView
