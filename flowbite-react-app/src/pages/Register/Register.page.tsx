c062e3f4a9eab1e541c756ce269ad069ca546e69
RoeeRabinovich <roeerabinovitz@gmail.com>
2025-06-18 16:15:48 +0300

    deleted node_modules

--- flowbite-react-app/src/pages/Register/Register.page.tsx
+++ flowbite-react-app/src/pages/Register/Register.page.tsx
@@ -184,7 +184,7 @@ const Register = () => {
           onClick={() => reset()}
           className="w-full bg-gray-300 hover:bg-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600"
         >
-          Reset Form.
+          Reset Form
         </Button>
       </form>
     </main>
